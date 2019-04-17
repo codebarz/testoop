53b983c9da381e894f60d5b46457fcc90f9d17c2
Tega Oke <oketegah@gmail.com>
2019-04-17 04:58:21 +0100

    Created file for test cases

---
+++ __test__/test.js
@@ -0,0 +1,7 @@
+const functions = require("../main");
+describe("to test test", () => {
+    test("should check", () => {
+        let ret = "hello";
+        expect(ret).toBe("hello");
+    })
+});
