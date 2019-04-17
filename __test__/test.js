1d84fb080658ebdcfebc1d292aa1857819847ed3
Tega Oke <oketegah@gmail.com>
2019-04-17 15:34:17 +0100

    user search function complete

--- __test__/test.js
+++ __test__/test.js
@@ -1,10 +1,15 @@
 const functions = require("../main");
-describe("To test if a user creation", () => {
+describe("To test if all normal user activities", () => {
     test("should check if a user was successfully created", () => {
         let createdUser = [];
         let newUser = new functions("whitehox", "white@gmail.com", "123", "admin");
         createdUser.push(JSON.stringify(newUser));
         console.log(createdUser);
         expect(createdUser).toEqual([ '{"username":"whitehox","email":"white@gmail.com","password":"123","access":"admin"}' ]);
+    });
+    test("should check if a  user can be checked with his ID", () => {
+        let result = functions.prototype.searchUserById(1, "admin", "admin");
+        console.log(result);
+        expected("tega").toBe("tega");
     })
 });
--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 let fs = require('fs');
+let loggedInUser = [];
 let dbData= JSON.parse(fs.readFileSync('db.json'));
-let db = [];
-function Users(username, email, password, access) {
+
+function Users (username, email, password, access) {
     this.username = username;
     this.email = email;
     this.password = password;
@@ -21,7 +22,7 @@ Users.prototype = {
                     password: this.password,
                     access: this.access
                 });
-                fs.writeFileSync('db.json', JSON.stringify(dbData, null, 2))
+                fs.writeFileSync('db.json', JSON.stringify(dbData, null, 2));
                 console.log("Your account has been successfully created");
             } else {
                 this.id = (dbData.users[dbData.users.length - 1].id) + 1;
@@ -36,7 +37,7 @@ Users.prototype = {
                 console.log("Your account has been successfully created");
             }
         }
-        else if (this.access === "admin" || this.access === "administrator") {
+        else if (this.access === "admin") {
             this.id = id;
             if (dbData.admin.length === 0) {
                 this.id = 1;
@@ -47,7 +48,7 @@ Users.prototype = {
                     password: this.password,
                     access: this.access
                 });
-                fs.writeFileSync('db.json', JSON.stringify(dbData, null, 2))
+                fs.writeFileSync('db.json', JSON.stringify(dbData, null, 2));
                 console.log("Your administrator account has been successfully created");
             } else {
                 this.id = (dbData.admin[dbData.admin.length - 1].id) + 1;
@@ -62,5 +63,50 @@ Users.prototype = {
                 console.log("Your administrator account has been successfully created");
             }
         }
+        else {
+            console.log("A user can not be created with this access level. Kindly use either admin or user");
+        }
+    },
+    searchUserById : function(id, access, searchType) {
+        this.id = id;
+        this.access = access;
+        this.searchType = searchType;
+        if(this.access === "admin") {
+            if (this.searchType === "admin") {
+                for (let i in dbData.admin) {
+                    if (dbData.admin[i].id === this.id) {
+                        console.log(dbData.admin[i]);
+                    }
+                    else {
+                        console.log("User does not exist");
+                    }
+                }
+            }
+            else if(this.searchType === "users") {
+                for (let i in dbData.admin) {
+                    if (dbData.admin[i].id === this.id) {
+                        console.log(dbData.admin[i]);
+                    }
+                    else {
+                        console.log("User does not exist");
+                    }
+                }
+            }
+            else {
+                console.log("Kindly search for either a user or an admin");
+            }
+        }
+    },
+    updateUser : function(id, username, access) {
+        this.id = id;
+        this.username = username;
+        this.access = access;
+
+        if(this.access === "admin") {
+
+        }
     }
 };
+//Users.prototype.searchUserById(1, "admin", "admin");
+
+module.exports = Users;
